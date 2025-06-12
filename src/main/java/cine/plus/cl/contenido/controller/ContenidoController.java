ae9338a75e9eddb315fbe4639cf33498eb98b5ab
Diego-Manriquez-DUOC <dieg.manriquez@duocuc.cl>
2025-06-12 18:44:18 -0400

    Implementación swagger

--- src/main/java/cine/plus/cl/contenido/controller/ContenidoController.java
+++ src/main/java/cine/plus/cl/contenido/controller/ContenidoController.java
@@ -10,6 +10,13 @@ import org.springframework.web.bind.annotation.*;
 
 import cine.plus.cl.contenido.model.Contenido;
 import cine.plus.cl.contenido.service.ContenidoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 
 
@@ -17,12 +24,19 @@ import cine.plus.cl.contenido.service.ContenidoService;
 
 @RestController
 @RequestMapping("/api/Contenido")
+@Tag(name = "Contenido", description = "Gestionar los Tipos de Contenidos.")
+
 public class ContenidoController {
 
     @Autowired
     private ContenidoService contenidoService;
 
     @GetMapping
+    @Operation(summary = "Obtiene los contenidos", description = "Obtiene una lista de Los Contenidos Disponibles.")
+    @ApiResponses(value = {
+    @ApiResponse(responseCode = "202", description = "OK, Operacion exitosa."),
+    @ApiResponse(responseCode = "404", description = "Contenido no encontrado")
+    })
     public ResponseEntity<List<Contenido>> listar() {
         List<Contenido> contenido = contenidoService.fetchAll();
 
@@ -33,9 +47,16 @@ public class ContenidoController {
         }
         // estado 202 ok.
         return ResponseEntity.ok(contenido);
+
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Obtener todas los Contenidos", description = "Obtiene una lista de todos los Contendidos Con ID.")
+    @ApiResponse(responseCode = "202", description = "Contenido encontrado, Operación exitosa")
+    @Parameter(description = "ID del contenido", required = true) 
+    
+
+
     public ResponseEntity<Contenido> buscar(@PathVariable Long id) {
         
         Contenido contenido = contenidoService.fetchById(id);
@@ -45,6 +66,11 @@ public class ContenidoController {
     }
     
     @PostMapping
+    @Operation(summary = "Guarda el contenido", description ="Guarda un nuevo contenido en el sistema y devuelve el contenido creado con su ID")
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Contenido a crear", required = true, content = @Content(schema = @Schema(implementation = Contenido.class)))
+
+
+    
     public ResponseEntity<Contenido> guardar(@RequestBody Contenido contenido) { 
         Contenido contenidoNuevo = contenidoService.save(contenido);
         return ResponseEntity.status(HttpStatus.CREATED).body(contenidoNuevo);
@@ -52,6 +78,10 @@ public class ContenidoController {
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Actualizar el Contenido", description = "Actualiza un contenido existente basado en el ID proporcionado y devuelve el contenido actualizado")
+    @Parameter(description = "ID del contenido", required = true) 
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Contenido a actualizar", required = true, content = @Content(schema = @Schema(implementation = Contenido.class)))
+    
     public ResponseEntity<Contenido> actualizar(
         @PathVariable Long id,
         @RequestBody Contenido contenido
@@ -77,6 +107,9 @@ public class ContenidoController {
     }
 
         @DeleteMapping("/{id}")
+        @Operation(summary = "Eliminar Contenido", description = "Elimina un contenido existente del sistema utilizando su ID")
+        @Parameter(description = "ID del contenido", required = true) 
+
         public ResponseEntity<?> eliminar(@PathVariable Long id){
             contenidoService.delete(id);
 
